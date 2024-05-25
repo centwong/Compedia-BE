ac28d72b42eae226772372d88d2be0a8ef0d63e6
CentWong <vinncentwong1@gmail.com>
2024-05-25 17:04:32 +0700

    initial commit

---
+++ src/main/java/cent/wong/compedia/CompediaApplication.java
@@ -0,0 +1,13 @@
+package cent.wong.compedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompediaApplication.class, args);
+	}
+
+}
